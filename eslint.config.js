cb7fc6f87cdf44c94abfa1cb23ee82e74c2a2638
snigdho48 <www.atik48@gmail.com>
2024-06-25 12:47:09 +0600

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    ignores: [
+      '.config/*',
+      'dist/*',
+      'node_modules/*',
+      'public/*',
+      'src/*',
+      'tailwind.config.js',
+      '*.json',
+      '*.md',
+      '*.yml',
+      '*.yaml',
+      '*.lock',
+      '*.lock.json',
+      '*.lock.yaml',
+      '*.lock.yml',
+      '*.lock.lock',
+      '*.lock.lock.json',
+      '*.lock.lock.yaml',
+      '*.lock.lock.yml',
+      '*.lock.lock.lock',
+    ],
+
+    rules: {
+      // ... any rules you want
+      semi: 'error',
+      'prefer-const': 'warn',
+      'no-unused-vars': 'warn',
+      'no-unused-expressions': 'warn',
+      'react/prop-types': 'off',
+      'no-undef': 'warn',
+      'import/no-unresolved': 'error',
+    },
+  },
+];
